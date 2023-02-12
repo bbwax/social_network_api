5af814293041b1328be1b9543a6a3b85d7f3ee1c
bwax <braxton.waxdeck@gmail.com>
2023-02-11 20:24:22 -0800

    completed user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,4 +21,44 @@ module.exports = {
                 return res.status(500).json(err);
             });
     },
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+          .select('-__v')
+          .then(async (user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with that ID' })
+              : res.json({
+                  user,
+                })
+          )
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+      },
+      deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user exists' })
+              : Thought.deleteMany(
+                { _id: { $in: user.thoughts } }
+                )
+          )
+          .then(() => res.json({ message: 'User and thoughts deleted!' }))
+          .catch((err) => res.status(500).json(err));
+      },
+      updateUser(req, res) {
+        User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $set: req.body },
+          { runValidators: true, new: true }
+        )
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with this id!' })
+              : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,8 +3,20 @@ const router = require('express').Router();
 const {
     createUser,
     getUsers,
+    getSingleUser,
+    deleteUser,
+    updateUser,
 } = require('../../controllers/userController');
 
-router.route('/').post(createUser).get(getUsers);
+router
+    .route('/')
+    .post(createUser)
+    .get(getUsers);
+    
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .delete(deleteUser)
+    .put(updateUser);
 
 module.exports = router;
